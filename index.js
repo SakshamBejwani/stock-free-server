039b61e815b5be6063eb2b018d1d8a7e1eb5cb72
Saksham Bejwani <sakshambejwani@gmail.com>
2022-01-21 20:57:00 +0530

    v0.0.0 Build

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import  mongoose  from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv'
+const app = express();
+dotenv.config()
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors())
+
+app.use('/posts', postRoutes)
+app.get('/', (req, res) => {
+    res.send('Navigate to /posts for REST API ussage: Saksham Bejwani')
+})
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log('Server Running on Port: '+ PORT)))
+    .catch((error) => console.log('Error', error.message))
+
