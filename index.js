0860498f57f4ccd41a799b627fd87cad02f7fb9d
Saksham Bejwani <sakshambejwani@gmail.com>
2023-04-10 19:22:29 +0530

    final-commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import postRoutes from './routes/posts.js';
 import dotenv from 'dotenv'
 const app = express();
 dotenv.config()
+
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors())
